1b25fd3725c4179bc788ae3c065f6a81184f9da0
SMYKI <officehiway@gmail.com>
2024-01-20 17:01:37 +0100

    Praca domowa dzień 4

--- ChallengeApp 21/ChallengeApp 21/Program.cs
+++ ChallengeApp 21/ChallengeApp 21/Program.cs
@@ -1,10 +1,22 @@
-﻿var myAge = 33;
-int myAge2 = 33;
-uint myVar = uint.MaxValue;
-long myVar2 = long.MaxValue;
-ulong myVar3 = ulong.MaxValue;
+﻿
+string name = "Ewa";
+string gender = "Kobieta";
+var age = 30;
+var age2 = 18;
 
-float myNumber = 12554;
-float myNumber2 = float.MaxValue;
-
-Console.WriteLine(myAge + myNumber);
+if (gender == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (gender != "Kobieta" && age2 < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine();
+}
